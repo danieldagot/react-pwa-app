f98140c28fbc7a63536a2ffbd940fbf58794363c
danieldagot <51596364+danieldagot@users.noreply.github.com>
2019-12-21 13:18:36 +0200

    get data

--- src/App.js
+++ src/App.js
@@ -3,23 +3,42 @@ import { BrowserRouter, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import Home from "./components/Home";
 import About from "./components/About";
+import ReactGoogleSheets from 'react-google-sheets'; 
+import Tabletop from 'tabletop';
 
+function showInfo(data, tabletop) {
+  alert('Successfully processed!')
+  console.log(data);
+}
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    Tabletop.init({
+      key: '1bwUidK8Ok86LfHYJUnzZWvTkW7FfhHlJsHY3GdKsYpI',
+      simpleSheet: true,
+      callback: data => {
+        console.log('google sheet data --->',data)
+        console.log("----------------------------------------");
+        this.setState({data:data})
+      },
+     // simpleSheet: true
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>React App</h2>
-        </div>
-        <BrowserRouter>
-          <div>
-            <Route path="/" exact component={Home} />
-            <Route path="/about" exact component={About} />
-          </div>
-        </BrowserRouter>
       </div>
     );
   }
 }
 export default App;
+
+
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
+
+
+const SHEET_ID = '1TK4yJK9Jj2cB3HxIA8SnRYC0oBDKyIOy3xEZZkGMzQw';
+const ACCESS_TOKEN = '328708097763-c10j1infothgirhoq5g25bmcou3vvrlh.apps.googleusercontent.com';
+ let getSheetValues = async () =>{
+  const request = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/A1:B5`,
+  {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${ACCESS_TOKEN}`  
+  }
+  });
+  const data = await request.json();
+  console.log(data.Sheet1.raw.feed.entry);
+  return data.Sheet1.raw.feed.entry;
+}
+
+
 const home = () => {
   return (
     <div title="Home">
+              <button onClick={getSheetValues()}>Get sheet values</button>
+      {JSON.stringify(getSheetValues)}
       <h1>Home Page</h1>
       <p>
         <Link to="/about">About</Link>
@@ -13,3 +34,8 @@ const home = () => {
 };
 
 export default home;
+
+
+
+
+  
