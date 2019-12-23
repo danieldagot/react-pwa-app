be3b6263e25b73bb060eff760d15e32cfc879dc3
danieldagot <51596364+danieldagot@users.noreply.github.com>
2019-12-23 19:40:50 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-// import React, { Component ,JsonTable } from "react";
-// import { BrowserRouter, Route } from "react-router-dom";
-// import logo from "./logo.svg";
-// import Home from "./components/Home";
-// import About from "./components/About";
-// import ReactGoogleSheets from 'react-google-sheets'; 
-// import Tabletop from 'tabletop';
-// import Raw from "./data" ; 
-// import "./app.css"
+import React, { Component ,JsonTable } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import logo from "./logo.svg";
+import Home from "./components/Home";
+import About from "./components/About";
+import ReactGoogleSheets from 'react-google-sheets'; 
+import Tabletop from 'tabletop';
+import Raw from "./data" ; 
+import "./app.css"
 class Tible extends Component {
   constructor() {
     super()
@@ -51,20 +51,16 @@ return     <tr>{vals.map(d =>  <td> {d}</td>)}</tr>
 
 
   
-import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import logo from "./logo.svg";
-import Home from "./components/Home";
-import About from "./components/About";
+// import React, { Component } from "react";
+// import { BrowserRouter, Route } from "react-router-dom";
+// import logo from "./logo.svg";
+// import Home from "./components/Home";
+// import About from "./components/About";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>React App</h2>
-        </div>
         <BrowserRouter>
           <div>
             <Route path="/" exact component={Tible} />
