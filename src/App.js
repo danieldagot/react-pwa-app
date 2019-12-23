40b651baac3c2e7647edb2953b232c602b6cf489
danieldagot <51596364+danieldagot@users.noreply.github.com>
2019-12-23 19:45:29 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component ,JsonTable } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import logo from "./logo.svg";
-import Home from "./components/Home";
+//import Home from "./components/Home";
 import About from "./components/About";
-import ReactGoogleSheets from 'react-google-sheets'; 
-import Tabletop from 'tabletop';
+//import ReactGoogleSheets from 'react-google-sheets'; 
+//import Tabletop from 'tabletop';
 import Raw from "./data" ; 
 import "./app.css"
 class Tible extends Component {
@@ -13,19 +13,6 @@ class Tible extends Component {
     this.state = {
       data: []
     }
-  }
-
-  componentDidMount() {
-    Tabletop.init({
-      key: '1bwUidK8Ok86LfHYJUnzZWvTkW7FfhHlJsHY3GdKsYpI',
-      simpleSheet: true,
-      callback: data => {
-        console.log('google sheet data --->',data)
-        console.log("----------------------------------------");
-        this.setState({data:data})
-      },
-     // simpleSheet: true
-    })
   }
 
 
